418b3baccf719831bce231e0832c5421affaee5d
Ender Invader <enderinvader.end@gmail.com>
2023-09-24 08:39:37 -0700

    Update GetOrientation.ino

--- GetOrientation.ino
+++ GetOrientation.ino
@@ -1,67 +1,66 @@
-void orientation(float orient[6])) {
-
-  //Create quaternion object and get quaternion data from BNO055
+void orientation(float orient[6]) {
+	//Create quaternion object and get quaternion data from BNO055
 	imu::Quaternion quat = bno.getQuat();
 
-  //Get conjugate of quaternion from BNO055
-  imu::Quaternion quatconj = quaternionConjugate(quat);
+	//Get conjugate of quaternion from BNO055
+	imu::Quaternion quatconj = quaternionConjugate(quat);
 
-  //Create 3-axis vector for angular velocity
-  imu::Vector<3> angvel = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+	//Create 3-axis vector for angular velocity
+	imu::Vector<3> angvel = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
 
-  //Assign conjugate quaternion values to variables
-  float qw = quatconj.w();
-  float qx = quatconj.x();
-  float qy = quatconj.y();
-  float qz = quatconj.z();
+	//Assign conjugate quaternion values to variables
+	float qw = quatconj.w();
+	float qx = quatconj.x();
+	float qy = quatconj.y();
+	float qz = quatconj.z();
 
-  //Get angular rate data from BNO055
-  float pitch_rate = angvel.x();
-  float yaw_rate = angvel.y();
-  float roll_rate = angvel.z();
+	//Get angular rate data from BNO055
+	float pitch_rate = angvel.x();
+	float yaw_rate = angvel.y();
+	float roll_rate = angvel.z();
 
-  //Convert quaternion data to Euler angles
-  float pitch, yaw, roll;
-  quaternionToEuler(qx,qy,qz,qw,&pitch,&yaw,&roll);
+	//Convert quaternion data to Euler angles
+	float pitch, yaw, roll;
+	quaternionToEuler(qx,qy,qz,qw,&pitch,&yaw,&roll);
 
-  //Assigning Pitch, Yaw, and Roll to elements of orient[] array
-  orient[1] = pitch;
-  orient[2] = yaw;
-  orient[3] = roll;
+	//Assigning Pitch, Yaw, and Roll to elements of orient[] array
+	orient[1] = pitch;
+	orient[2] = yaw;
+	orient[3] = roll;
 
-  //Assigning Pitch rate, Yaw rate, and Roll rate to elements of orient[] array
-  orient[4] = pitch_rate;
-  orient[5] = yaw_rate;
-  orient[6] = roll_rate;
+	//Assigning Pitch rate, Yaw rate, and Roll rate to elements of orient[] array
+	orient[4] = pitch_rate;
+	orient[5] = yaw_rate;
+	orient[6] = roll_rate;
 
-  //Plotting Pitch, Yaw, and Roll
-  plotOrientation(pitch,yaw,roll);
+	//Plotting Pitch, Yaw, and Roll
+	plotOrientation(pitch,yaw,roll);
 }
 
-void quaternionToEuler(float qx, float qy, float qz, float qw, float *pitch, float *yaw, float *roll) {
-  // Convert quaternion to Euler angles (pitch, roll, and yaw) in degrees
-  *pitch = atan2(2.0 * (qw * qx + qy * qz), 1.0 - 2.0 * (qx * qx + qy * qy)) * 180.0 / M_PI;
-  *yaw = asin(2.0 * (qw * qy - qz * qx)) * 180.0 / M_PI; 
-  *roll = atan2(2.0 * (qw * qz + qx * qy), 1.0 - 2.0 * (qy * qy + qz * qz)) * 180.0 / M_PI;
+void quaternionToEuler(float qx, float qy, float qz, float qw, float* pitch, float* yaw, float* roll) {
+	// Convert quaternion to Euler angles (pitch, roll, and yaw) in degrees
+	*pitch = atan2(2.0 * (qw * qx + qy * qz), 1.0 - 2.0 * (qx * qx + qy * qy)) * 180.0 / M_PI;
+	*yaw = asin(2.0 * (qw * qy - qz * qx)) * 180.0 / M_PI; 
+	*roll = atan2(2.0 * (qw * qz + qx * qy), 1.0 - 2.0 * (qy * qy + qz * qz)) * 180.0 / M_PI;
 }
 
 imu::Quaternion quaternionConjugate(imu::Quaternion quat) {
-  imu::Quaternion conj;
-  conj.w() = quat.w();
-  conj.x() = -quat.x();
-  conj.y() = -quat.y();
-  conj.z() = -quat.z();
-  return conj;  
+	imu::Quaternion conj;
+	conj.w() = quat.w();
+	conj.x() = -quat.x();
+	conj.y() = -quat.y();
+	conj.z() = -quat.z();
+	return conj;  
 }
 
 void plotOrientation(float pitch, float yaw, float roll) {
-  // Display angles in the Serial Plotter
-  Serial.print("Pitch:");
-  Serial.print(pitch);
-  Serial.print(",");
-  Serial.print("Yaw:");
-  Serial.println(yaw);
-  Serial.print(",");
-  Serial.print("Roll:");
-  Serial.println(roll);
+	// Display angles in the Serial Plotter
+	Serial.print("Pitch:");
+	Serial.print(pitch);
+	Serial.print(",");
+	Serial.print("Yaw:");
+	Serial.println(yaw);
+	Serial.print(",");
+	Serial.print("Roll:");
+	Serial.println(roll);
 }
