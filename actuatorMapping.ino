82242a8599bddfd68247aaa7f66605b038ad3172
DMNgineer <144561615+DMNgineer@users.noreply.github.com>
2023-12-01 16:08:58 -0800

    Update actuatorMapping.ino
    Added code to slow down the other two actuators so that they finish driving at the same time as the slowest, added for loops to shorten the code

--- actuatorMapping.ino
+++ actuatorMapping.ino
@@ -8,7 +8,8 @@
 struct actuator {
       byte forwardPin, backwardPin;
       double currentLength, displacement, driveTime;
-  } actuator1, actuator2, actuator3;
+      int pwm;
+};
 
 void setup() {
   pinMode(actuator1Forward, OUTPUT);
@@ -17,83 +18,80 @@ void setup() {
   pinMode(actuator2Backward, OUTPUT);
   pinMode(actuator3Forward, OUTPUT);
   pinMode(actuator3Backward, OUTPUT);
-  actuator1 = {actuator1Forward, actuator1Backward};
-  actuator2 = {actuator2Forward, actuator2Backward};
-  actuator3 = {actuator3Forward, actuator3Backward};
 }
 
 void loop() {
-  lengthInputs = getLengths(); // inputs for getLengths taken from PIDcontrol.ino
-  driveActuators(lengthInputs[1], lengthInputs[2], lengthInputs[3]);
+  float lengthInputs[3] = getLengths(); // inputs for getLengths taken from PIDcontrol.ino
+  driveActuators(lengthInputs);
 }
 
-double getLengths(double inputPitch, double inputYaw) {
+float getLengths(float inputPitch, float inputYaw) {
 	
 }
 
-int compare(const actuator* actuator1, const actuator* actuator2) {
+int compareDriveTimes(const actuator* actuator1, const actuator* actuator2) {
   return actuator1->driveTime - actuator2->driveTime;
 }
 
-byte actuatorDirection(double displacement, byte forwardPin, byte backwardPin) {
+byte actuatorDirection(float displacement, byte forwardPin, byte backwardPin) {
   if (displacement >= 0)
     return forwardPin;
   else
     return backwardPin;
 }
 
-void driveActuators(double length1, double length2, double length3) {
-  /*  Assume constants are acceleration and max speed, must be experimentally found
-  *  Limitations: error propagates over time since there's no way to check position past initial conditions
-  */ 
+void driveActuators(float* lengthInputs) {
+	/*  Assume constants are acceleration and max speed, must be experimentally found
+	 *  Limitations: error propagates over time since there's no way to check position past initial conditions
+	 */ 
   // Initial conditions
   static int iteration = 0;
   // TODO: Find actual max speed and acceleration time
-  double maxSpeed = 3; // m/s
-  double accel = 3; // m/s^2
-  double accelTime = maxSpeed/accel; // s
-  double lengthTransient = 0.5*accel*pow(accelTime, 2); // m
-  if (iteration == 0) {
-    // TODO: Find actual neutral lengths
-    actuator1.currentLength = 1; // m, same length for all actuators when motor is straight
-    actuator2.currentLength = 1; // m, same length for all actuators when motor is straight
-    actuator3.currentLength = 1; // m, same length for all actuators when motor is straight
-  }
-  actuator1.displacement = length1 - actuator1.currentLength;
-  actuator2.displacement = length2 - actuator2.currentLength;
-  actuator3.displacement = length3 - actuator3.currentLength;
+	float maxSpeed = 3; // m/s
+	float accel = 3; // m/s^2
+  float accelTime = maxSpeed/accel; // s
+  float lengthTransient = 0.5*accel*pow(accelTime, 2); // m
+  actuator actuators[3];
+  actuators[1].forwardPin = actuator1Forward;
+  actuators[1].backwardPin = actuator1Backward;
+  actuators[2].forwardPin = actuator2Forward;
+  actuators[2].backwardPin = actuator2Backward;
+  actuators[3].forwardPin = actuator3Forward;
+  actuators[3].backwardPin = actuator3Backward;
+  for (int i = 1; i < 4; ++i) {
+    if (iteration == 0) {
+      // TODO: Find actual neutral lengths
+	    actuators[i].currentLength = 1; // m, same length for all actuators when motor is straight
+    }
+    actuators[i].displacement = lengthInputs[i] - actuators[i].currentLength;
 
-  // Calculating drive times for actuators at max power (function of desired length and current length)
-  if (length1 >= lengthTransient) {
-    //length1 = currentLength1 + lengthTransient*t/accelTime; // m
-    actuator1.driveTime = abs((length1 - actuator1.currentLength))*accelTime/lengthTransient*1000; // ms
-  } else {
-    // length1 = currentLength1 + lengthTransient + maxSpeed*(t-accelTime); // m
-    actuator1.driveTime = (abs((length1 - actuator1.currentLength - lengthTransient))/maxSpeed + accelTime)*1000; // ms
+	  // Calculating drive times for actuators at max power (function of desired length and current length)
+    if (lengthInputs[i] >= lengthTransient) {
+      //length1 = currentLength1 + lengthTransient*t/accelTime; // m
+      actuators[i].driveTime = abs((lengthInputs[i] - actuators[i].currentLength))*accelTime/lengthTransient*1000; // ms
+    } else {
+      // length1 = currentLength1 + lengthTransient + maxSpeed*(t-accelTime); // m
+      actuators[i].driveTime = (abs((lengthInputs[i] - actuators[i].currentLength - lengthTransient))/maxSpeed + accelTime)*1000; // ms
+    }
   }
-  if (length2 >= lengthTransient) {
-    //length1 = currentLength1 + lengthTransient*t/accelTime; // m
-    actuator2.driveTime = abs((length2 - actuator2.currentLength))*accelTime/lengthTransient*1000; // ms
-  } else {
-    // length1 = currentLength1 + lengthTransient + maxSpeed*(t-accelTime); // m
-    actuator2.driveTime = (abs((length2 - actuator2.currentLength - lengthTransient))/maxSpeed + accelTime)*1000; // ms
-  }
-  if (length3 >= lengthTransient) {
-    //length1 = currentLength1 + lengthTransient*t/accelTime; // m
-    actuator3.driveTime = abs((length3 - actuator3.currentLength))*accelTime/lengthTransient*1000; // s
-  } else {
-    // length1 = currentLength1 + lengthTransient + maxSpeed*(t-accelTime); // m
-    actuator3.driveTime = (abs((length3 - actuator3.currentLength - lengthTransient))/maxSpeed + accelTime)*1000; // s
-  }
-
   // Assign drive timings and associated actuators from shortest to longest
-  actuator actuators[3] = {actuator1, actuator2, actuator3};
+  qsort(actuators, 3, sizeof(actuator), compareDriveTimes);
+
+  // Slowing down the other two actuators to have the same drive time as the longest one
+  actuators[1].pwm = actuators[1].driveTime/actuators[3].driveTime*255;
+  actuators[2].pwm = actuators[2].driveTime/actuators[3].driveTime*255;
+  actuators[3].pwm = 255;
+  // Assume acceleration and max velocity remain the same (duty cycles)
+  actuators[1].driveTime = actuators[1].driveTime / actuators[1].pwm * 255; // ms
+  actuators[2].driveTime = actuators[2].driveTime / actuators[2].pwm * 255; // ms
+
+  // Sort by drive time again for stopping the actuators at the right time
   qsort(actuators, 3, sizeof(actuator), compareDriveTimes);
 
   // Driving actuators
-  analogWrite(actuatorDirection((length1-actuator1.currentLength), actuator1Forward, actuator1Backward), 255);
-  analogWrite(actuatorDirection((length2-actuator2.currentLength), actuator2Forward, actuator2Backward), 255);
-  analogWrite(actuatorDirection((length3-actuator3.currentLength), actuator3Forward, actuator3Backward), 255);
+  analogWrite(actuatorDirection(actuators[1].displacement, actuators[1].forwardPin, actuators[1].backwardPin), actuators[1].pwm);
+  analogWrite(actuatorDirection(actuators[2].displacement, actuators[2].forwardPin, actuators[2].backwardPin), actuators[2].pwm);
+  analogWrite(actuatorDirection(actuators[3].displacement, actuators[3].forwardPin, actuators[3].backwardPin), actuators[3].pwm);
   delay(actuators[1].driveTime);
   analogWrite(actuatorDirection(actuators[1].displacement, actuators[1].forwardPin, actuators[1].backwardPin), 0);
   delay(actuators[2].driveTime);
@@ -102,8 +100,8 @@ void driveActuators(double length1, double length2, double length3) {
   analogWrite(actuatorDirection(actuators[3].displacement, actuators[3].forwardPin, actuators[3].backwardPin), 0);
 
   // Set initial conditions for next iteration
-  actuator1.currentLength = length1; // m
-  actuator2.currentLength = length2; // m
-  actuator3.currentLength = length3; // m
+  actuators[1].currentLength = lengthInputs[1]; // m
+  actuators[2].currentLength = lengthInputs[2]; // m
+  actuators[3].currentLength = lengthInputs[3]; // m
   ++iteration;
 }
